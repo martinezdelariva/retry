0015d3a0c12c03b0a9b1fe1820117097e26da5c7
José Luis Martínez de la Riva <martinezdelariva@gmail.com>
2019-02-23 16:50:19 +0100

    concurrency implementation and test

--- pkg/retry/retry.go
+++ pkg/retry/retry.go
@@ -4,14 +4,16 @@ import (
 	"bytes"
 	"context"
 	"os/exec"
+	"sync"
 	"time"
 )
 
-// retrySleep proxy to function time.After and is overridden in testing to not depend on time
-var retrySleep = time.After
+// sleep proxy to function time.After and is overridden in testing to not depend on time
+var sleep = time.After
 
-// Command returns a retry which encapsulates name and arguments of the command to be executed with
-// the configuration policies for retries and context for cancellation.
+// Command returns a retry which encapsulates name and arguments of the command and configuration
+// policies used for every execution. The context is used to kill ongoing command execution and
+// to cancel next retries.
 func Command(ctx context.Context, name string, arg []string, config Config) *retry {
 	return &retry{
 		name:   name,
@@ -22,8 +24,9 @@ func Command(ctx context.Context, name string, arg []string, config Config) *ret
 }
 
 type Config struct {
-	Max   int
-	Sleep time.Duration
+	Max         int
+	Sleep       time.Duration
+	Concurrency int
 }
 
 type retry struct {
@@ -35,40 +38,88 @@ type retry struct {
 
 // Run starts the executions of the same command and returns a channel where results are sent.
 // Each result contains *exec.Cmd already executed, total time.Duration of the execution and
-// any error returns from *exec.Cmd (Err are the ones returned by *exec.Cmd)
+// any error returns from *exec.Cmd (Err are the ones returned by *exec.Cmd).
+// There is no guarantee that *exec.Cmd is not nil, due the command is only created just before
+// to be executed, and the retry could be waiting for concurrency or sleeping reasons.
 func (r *retry) Run() <-chan Result {
 	out := make(chan Result)
 
 	go func() {
 		defer close(out)
+
+		// look path
+		if _, err := exec.LookPath(r.name); err != nil {
+			out <- Result{Command: nil, RealTime: 0, Err: err}
+			return
+		}
+
+		var once sync.Once
+		var wg sync.WaitGroup
+		wg.Add(r.config.Max)
+
+		sem := make(chan struct{}, r.config.Concurrency)
+		defer close(sem)
+
 		for i := 0; i < r.config.Max; i++ {
-			// sleep or done
-			if i != 0 {
+			go func() {
+				defer func() {
+					wg.Done()
+				}()
+
+				// concurrency
 				select {
-				case <-retrySleep(r.config.Sleep):
+				case sem <- struct{}{}: // get token
 				case <-r.cxt.Done():
-					out <- Result{Command: nil, RealTime: 0, Err: r.cxt.Err()}
+					once.Do(func() {
+						out <- Result{Command: nil, RealTime: 0, Err: r.cxt.Err()}
+					})
 					return
 				}
-			}
 
-			now := time.Now()
-			stdout := &bytes.Buffer{}
-			stderr := &bytes.Buffer{}
-			cmd := exec.CommandContext(r.cxt, r.name, r.arg...)
-			cmd.Stdout = stdout
-			cmd.Stderr = stderr
-			err := cmd.Run()
+				// sleep
+				select {
+				case <-sleep(r.config.Sleep):
+				case <-r.cxt.Done():
+					once.Do(func() {
+						out <- Result{Command: nil, RealTime: 0, Err: r.cxt.Err()}
+					})
+					return
+				}
 
-			out <- Result{Command: cmd, RealTime: time.Since(now), Err: err}
+				// exec
+				result := r.retry()
 
-			if _, ok := err.(*exec.Error); ok {
-				return
-			}
+				// free token
+				<-sem
+
+				if r.cxt.Err() != nil {
+					once.Do(func() {
+						out <- result
+					})
+				} else {
+					out <- result
+				}
+			}()
 		}
+
+		wg.Wait()
 	}()
 
 	return out
+}
+
+func (r *retry) retry() Result {
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	now := time.Now()
+
+	cmd := exec.CommandContext(r.cxt, r.name, r.arg...)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err := cmd.Run()
+
+	duration := time.Since(now)
+	return Result{Command: cmd, RealTime: duration, Err: err}
 }
 
 type Result struct {
